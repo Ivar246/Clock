36e35aba5a9cc79fb6736fd9a58029871dea42c5
IVAR AHTSERHS <ravistha869@gmail.com>
2022-12-30 22:41:17 +0545

    add audio feature

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const hourHand = document.querySelector(".hour-hand");
 
 function setDate() {
   const now = new Date();
+  const audio = new Audio("tiktik.m4a ");
 
   let hours = now.getHours();
   const minutes = now.getMinutes();
@@ -16,11 +17,12 @@ function setDate() {
   const hoursDegrees = (360 / 12) * hours + 90 + hoursOffsetDegree;
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 
-  let minutesOffsetDegree = (seconds / 60) * 5;
+  let minutesOffsetDegree = (seconds / 60) * 6;
   const minutesDegrees = (minutes / 60) * 360 + 90 + minutesOffsetDegree;
   minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
   const secondsDegrees = (seconds / 60) * 360 + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  audio.play();
 }
 setInterval(setDate, 1000);
